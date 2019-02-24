70580cbe2e0534f9cee356b35dc5b6dc192bbfee
andriy.tsovma@aemcorp.com <andriy.tsovma@aemcorp.com>
2019-02-23 22:46:26 -0600

    initial

---
+++ app/src/main/java/com/texpilot/medcard/mCard.java
@@ -0,0 +1,13 @@
+package com.texpilot.medcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_m_card);
+    }
+}
