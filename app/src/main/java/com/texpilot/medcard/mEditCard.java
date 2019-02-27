9c7b030f1e46170216e5195514cb7a740a7066f0
andriy.tsovma@aemcorp.com <andriy.tsovma@aemcorp.com>
2019-02-27 07:01:10 -0600

    validators optimization

--- app/src/main/java/com/texpilot/medcard/mEditCard.java
+++ app/src/main/java/com/texpilot/medcard/mEditCard.java
@@ -42,42 +42,32 @@ public class mEditCard extends AppCompatActivity {
     private boolean ValidateEntryFields() {
         boolean result = false;
         TextView eKey1 = findViewById(R.id.eKey1);
-        TextView eVal1 = findViewById(R.id.eVal1);
         String K1 = eKey1.getText().toString();
-        String V1 = eVal1.getText().toString();
 
         TextView eKey2 = findViewById(R.id.eKey2);
-        TextView eVal2 = findViewById(R.id.eVal2);
         String K2 = eKey2.getText().toString();
-        String V2 = eVal2.getText().toString();
 
         TextView eKey3 = findViewById(R.id.eKey3);
-        TextView eVal3 = findViewById(R.id.eVal3);
         String K3 = eKey3.getText().toString();
-        String V3 = eVal3.getText().toString();
 
         TextView eKey4 = findViewById(R.id.eKey4);
-        TextView eVal4 = findViewById(R.id.eVal4);
         String K4 = eKey4.getText().toString();
-        String V4 = eVal4.getText().toString();
 
         TextView eKey5 = findViewById(R.id.eKey5);
-        TextView eVal5 = findViewById(R.id.eVal5);
         String K5 = eKey5.getText().toString();
-        String V5 = eVal5.getText().toString();
         if (!K1.isEmpty() || !K2.isEmpty() || !K3.isEmpty() || !K4.isEmpty() || !K5.isEmpty()) {
             result = true;
         }
         return result;
     };
 
-
+    // save to DB
     private void SavetoDB(){
         SQLiteDatabase database = new MedCardDBSQLiteHelper(this).getWritableDatabase();
         Long MedCardId;
         long date;
         ContentValues values = new ContentValues();
-        // insert MedCardId
+        // generate new MedCardId
         try {
             Calendar calendar = Calendar.getInstance();
             SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss:S");
@@ -96,31 +86,68 @@ public class mEditCard extends AppCompatActivity {
             Toast.makeText(this, "Date is in the wrong format", Toast.LENGTH_LONG).show();
             return;
         }
-        // if MedCardId insert is successful, then insert pairs of key->value first row
+
+        // if MedCardId insert is successful, then insert pairs of key->value row 1
         values = new ContentValues();
         Integer i = 0;
         TextView eKey = findViewById(R.id.eKey1);
         TextView eVal = findViewById(R.id.eVal1);
         String K = eKey.getText().toString();
         String V = eVal.getText().toString();
-        if (K != "" && V != "") {
-            values.put(MedCardDBContract.CardContent.COLUMN_MEDCARDID,MedCardId);
-            values.put(MedCardDBContract.CardContent.COLUMN_KEY, K);
-            values.put(MedCardDBContract.CardContent.COLUMN_VALUE, V);
-            values  .put(MedCardDBContract.CardContent.COLUMN_CREATEDDATE, date);
-        }
-        // second row
-        eKey = findViewById(R.id.eKey2);
-        eVal = findViewById(R.id.eVal2);
-        K = eKey.getText().toString();
-        V = eVal.getText().toString();
-        if (K != "" && V != "") {
+        if (!K.isEmpty()) {
             values.put(MedCardDBContract.CardContent.COLUMN_MEDCARDID,MedCardId);
             values.put(MedCardDBContract.CardContent.COLUMN_KEY, K);
             values.put(MedCardDBContract.CardContent.COLUMN_VALUE, V);
             values  .put(MedCardDBContract.CardContent.COLUMN_CREATEDDATE, date);
         }
 
+        // row 2
+        eKey = findViewById(R.id.eKey2);
+        eVal = findViewById(R.id.eVal2);
+        K = eKey.getText().toString();
+        V = eVal.getText().toString();
+        if (!K.isEmpty()) {
+            values.put(MedCardDBContract.CardContent.COLUMN_MEDCARDID,MedCardId);
+            values.put(MedCardDBContract.CardContent.COLUMN_KEY, K);
+            values.put(MedCardDBContract.CardContent.COLUMN_VALUE, V);
+            values  .put(MedCardDBContract.CardContent.COLUMN_CREATEDDATE, date);
+        }
+
+        // row 3
+        eKey = findViewById(R.id.eKey3);
+        eVal = findViewById(R.id.eVal3);
+        K = eKey.getText().toString();
+        V = eVal.getText().toString();
+        if (!K.isEmpty()) {
+            values.put(MedCardDBContract.CardContent.COLUMN_MEDCARDID,MedCardId);
+            values.put(MedCardDBContract.CardContent.COLUMN_KEY, K);
+            values.put(MedCardDBContract.CardContent.COLUMN_VALUE, V);
+            values  .put(MedCardDBContract.CardContent.COLUMN_CREATEDDATE, date);
+        }
+
+        // row 4
+        eKey = findViewById(R.id.eKey4);
+        eVal = findViewById(R.id.eVal4);
+        K = eKey.getText().toString();
+        V = eVal.getText().toString();
+        if (!K.isEmpty()) {
+            values.put(MedCardDBContract.CardContent.COLUMN_MEDCARDID,MedCardId);
+            values.put(MedCardDBContract.CardContent.COLUMN_KEY, K);
+            values.put(MedCardDBContract.CardContent.COLUMN_VALUE, V);
+            values  .put(MedCardDBContract.CardContent.COLUMN_CREATEDDATE, date);
+        }
+
+        // row 5
+        eKey = findViewById(R.id.eKey5);
+        eVal = findViewById(R.id.eVal5);
+        K = eKey.getText().toString();
+        V = eVal.getText().toString();
+        if (!K.isEmpty()) {
+            values.put(MedCardDBContract.CardContent.COLUMN_MEDCARDID,MedCardId);
+            values.put(MedCardDBContract.CardContent.COLUMN_KEY, K);
+            values.put(MedCardDBContract.CardContent.COLUMN_VALUE, V);
+            values  .put(MedCardDBContract.CardContent.COLUMN_CREATEDDATE, date);
+        }
 
         // write to the DB
         try {
