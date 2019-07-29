c9d17ecee2d3d4c023aaf233812b390dc6169916
andriy.tsovma@aemcorp.com <andriy.tsovma@aemcorp.com>
2019-07-29 11:18:33 -0500

    widget added

--- app/src/main/java/com/texpilot/medcard/MedCardWidget.java
+++ app/src/main/java/com/texpilot/medcard/MedCardWidget.java
@@ -1,9 +1,12 @@
 package com.texpilot.medcard;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 /**
  * Implementation of App Widget functionality.
@@ -20,13 +23,26 @@ public class MedCardWidget extends AppWidgetProvider {
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+
+
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
+        /*for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
+            Toast.makeText(context, "No values to enter", Toast.LENGTH_LONG).show();
+        }*/
+        for (int i = 0; i < appWidgetIds.length; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            Intent intent = new Intent(context, MedCardWidget.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.med_card_widget);
+            views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
